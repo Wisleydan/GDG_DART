975ce7cf80ac9e4da7cb155a3f9a53f5fc7ab180
Wisleydan <weisleydan49@hotmail.com>
2025-10-18 08:11:53 -0400

    If Else

---
+++ check_positive.dart
@@ -0,0 +1,13 @@
+void main() {
+int p;
+p = 6;
+if (p > 0) {
+  print("The number $p is positive");
+}
+else if (p < 0) {
+  print("The number $p is negative");
+}
+else {
+  print("The number $p is zero");
+}
+}
---
+++ voting.dart
@@ -0,0 +1,9 @@
+void main() {
+  int age = 17;
+  if (age >= 18) {
+    print("You are eligible for voting!");
+  }
+  else {
+    print("You are not eligible for voting!");
+  }
+}
