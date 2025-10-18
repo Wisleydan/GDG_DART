a13d45e1d8c2874fd26cdb754f6334e3a4b068ee
Wisleydan <weisleydan49@hotmail.com>
2025-10-18 08:10:53 -0400

    Add files via upload
    Combined data and Control flow

---
+++ even_odd.dart
@@ -0,0 +1,7 @@
+void main() {
+  int p = 101;
+  if (p % 2 == 0) {
+    print("The number $p is an even number!");
+    }else {
+    print("The number $p is an odd number!");}
+}
---
+++ num_cat.dart
@@ -0,0 +1,10 @@
+void main() {
+int  number = 12;
+if (number >= 1 && number <= 10) {
+  print("The number $number is a small number.");
+} else if (number >= 11 && number <= 100) {
+  print("The number $number is a medium number.");
+} else{
+  print("The number $number is a large number.");
+}
+}
